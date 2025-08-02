82bf079d9e2f58b7f6172b7ec41a76f72d5e2466
Prakhar Sharma <prakharsharma2506@gmail.com>
2025-08-03 04:32:58 +0530

    Update programs/ralli-bet/programs/ralli-bet/src/state/line.rs
    Co-authored-by: jacksturt <117492794+jacksturt@users.noreply.github.com>

--- programs/ralli-bet/programs/ralli-bet/src/state/line.rs
+++ programs/ralli-bet/programs/ralli-bet/src/state/line.rs
@@ -4,7 +4,7 @@ use anchor_lang::prelude::*;
 pub struct Line {
     pub stat_id: u16,
     pub predicted_value: u64,
-    pub actual_value: u64,
+    pub actual_value: Option<u64>,
     pub athlete_id: Pubkey,
     pub starts_at: i64,
     pub result: Option<Direction>,
