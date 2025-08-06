dca4e1e0563797e552a512e3d679f4c451bc7036
zsh28 <zeeshan8210@outlook.com>
2025-08-07 01:28:07 +0300

    add athletes api endpoint added

---
+++ apps/server/src/athletes/dto/create-athlete.dto.ts
@@ -0,0 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber, IsOptional, Min, Max } from 'class-validator';
+
+export class CreateAthleteDto {
+  @ApiProperty({ description: 'Athlete name' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ description: 'Team name' })
+  @IsString()
+  team: string;
+
+  @ApiProperty({ description: 'Player position' })
+  @IsString()
+  position: string;
+
+  @ApiProperty({
+    description: 'Jersey number',
+    minimum: 0,
+    maximum: 99,
+    example: 0,
+  })
+  @IsNumber()
+  @Min(0)
+  @Max(99)
+  jerseyNumber: number;
+
+  @ApiProperty({
+    description: 'Player age',
+    minimum: 16,
+    maximum: 50,
+    example: 0,
+  })
+  @IsNumber()
+  @Min(16)
+  @Max(50)
+  age: number;
+
+  @ApiProperty({ description: 'Player picture URL', required: false })
+  @IsOptional()
+  @IsString()
+  picture?: string;
+}
