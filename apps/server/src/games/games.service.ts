ee08c80ae787dd8731a7e2b88be8dc0e9aeada21
qhristen <odeyxtian@gmail.com>
2025-09-01 19:59:18 +0100

    fix error

--- apps/server/src/games/games.service.ts
+++ apps/server/src/games/games.service.ts
@@ -544,8 +544,7 @@ export class GamesService {
 
   async ensureUserOwnsGame(gameId: string, userId: string) {
     const game = await this.db.query.games.findFirst({
-      where: (g, { eq }) => eq(game.id, gameId),
-      where: (g, { eq }) => eq(game.id, gameId),
+      where: eq(games.id, gameId),
     });
 
     if (!game) throw new NotFoundException('Game not found');
