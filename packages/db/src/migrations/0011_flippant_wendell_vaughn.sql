14e3e99ab47066c40cb3ab63bbc8084bf6e1ea90
Ahindra D. <official.ahindra@gmail.com>
2025-09-12 03:42:23 +0530

    fix3:skipping-duplicate-columns-creation

--- packages/db/src/migrations/0011_flippant_wendell_vaughn.sql
+++ packages/db/src/migrations/0011_flippant_wendell_vaughn.sql
@@ -1,2 +1,17 @@
-ALTER TABLE "matchups" ADD COLUMN "odds_api_event_id" varchar;--> statement-breakpoint
-ALTER TABLE "stats" ADD COLUMN "odds_api_stat_name" varchar;
+DO $$ 
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns 
+        WHERE table_name = 'matchups' 
+        AND column_name = 'odds_api_event_id'
+    ) THEN
+        ALTER TABLE "matchups" ADD COLUMN "odds_api_event_id" varchar;
+    END IF;
+    IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns 
+        WHERE table_name = 'stats' 
+        AND column_name = 'odds_api_stat_name'
+    ) THEN
+        ALTER TABLE "stats" ADD COLUMN "odds_api_stat_name" varchar;
+    END IF;
+END $$;
